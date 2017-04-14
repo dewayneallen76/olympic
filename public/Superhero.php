13fabdda92f60142a5f7b3f71fe99cb6828814b5
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-14 13:56:12 -0500

    added method, and output to show inheritance

--- public/Superhero.php
+++ public/Superhero.php
@@ -1,18 +1,33 @@
 <?php 
 require __DIR__ . "/person.php";
 
-class SuperHero extends Person 
-{
-	public $alterEgo;
+// Best Practices: 
+// 1. Each class shoudl have its own file where the filename is the same as the class name
+// 2. Automobile.php, Car.php, Truck.php
+// 3. Classes/Objects are a way of organizing functionality. 
 
-	public function alterEgo()
-	{
-		return $this->alterEgo;
-	}
+class Superhero extends Person
+{
+    public $pseudonym;
+    public $capeColor;
+
+    public function alterEgo()
+    {
+        return 'Top Secret Alternate Ego: ' . $this->fullName();
+    }
+
+    public function hasCape()
+    {
+        return !empty($this->capeColor);
+    }
 }
 
-$person = new Person('Dewayne', 'Allen');
-echo $person->fullName(), PHP_EOL;
+$superman = new Superhero('Clark', 'Kent');
+$superman->pseudonym = 'Superman';
+$superman->capeColor = 'red';
+
+echo $superman->alterEgo(), PHP_EOL;
+var_dump($superman->hasCape());
 
 
  ?>
