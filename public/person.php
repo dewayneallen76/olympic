6230bec2de8fbe6680158508130416b443147350
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-14 12:07:06 -0500

    added construct method

--- public/person.php
+++ public/person.php
@@ -4,9 +4,18 @@ class Person
 	public $name;
 	public $languages;
 
-	
+	public function __construct($name, array $languages)
+	{
+		$this->name = $name;
+		$this->languages = $languages;
+		
+	}
 }
 
+$person = new Person('Dewayne',['HTHL', 'PHP', 'MySQL']); // Enforce rules when you construct an object
+
+echo $person->name, PHP_EOL;
+print_r($person->languages) . PHP_EOL;
 
 
  ?>
