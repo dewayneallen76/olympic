e94aa0919c76a996abb09360de20292491dc65d9
Dewayne Allen <dewayneallen76@gmail.com>
2017-04-17 10:02:51 -0500

    working with visibility on classes exercises changing to private and protected

--- public/shapes_test.php
+++ public/shapes_test.php
@@ -4,11 +4,18 @@ require __DIR__ . '/Rectangle.php';
 require __DIR__ . '/Square.php';
 
 
-$test = new Rectangle(5,5);
+$test = new Rectangle(3,5);
 echo "The area is: " . $test->area() .PHP_EOL;
 
-$test2 = new Square(9);
+$test2 = new Square(4);
 echo "The perimeter is: " . $test2->perimeter() . PHP_EOL;
+
+$test3 = new Square(5);
+echo $test3->area() . PHP_EOL;
+
+$test4 = new Rectangle(4);
+echo $test4->area() . PHP_EOL;
+
 
 
  ?>
